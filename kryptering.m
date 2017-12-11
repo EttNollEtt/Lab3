bfdd171763ddf5175283fe65a084d73e45bb418a
EttNollEtt <33880192+EttNollEtt@users.noreply.github.com>
2017-12-11 14:34:23 +0100

    Update kryptering.m

--- kryptering.m
+++ kryptering.m
@@ -5,44 +5,20 @@ close all
 % 2017-12-02 Karl Hallberg
 % 2017-12-09 Kevin Kihlström
 
-% Det som behövs göras som jag inte löste var att göra en pie() av de 10
-% mest frekventa bokstäverna i text-filen, min funktion slutar efter att
-% bokstäverna slutar vara jättefrekventa. Någon sorts funktion som räknar
-% igenom alla elementen krävs och sedan göra ett diagram av det
-
 % Läser in en krypterad fil och gör den till
 % stora bokstäver samt konverterar dem till ASCII
+
 fid = fopen('krypterad3.txt','rt');
 textOriginal = fscanf(fid,'%c',Inf);
 textUpper = upper(textOriginal);
 textAsc = double(textUpper);
-textTest = textAsc; % använder mig utav textTest för att kunna dekryptera från originaltext sedan
-textTestPaj = textAsc; %skapar en ny för att underlätta
-
-% Kom på att mellanslag är 32 i ASCII så tar bort
-% dem från min array jag modifierar men den verkar EJ FUNGERA det verkar
-% inte behövas men det kan komma upp som en frekvent bokstav i små texter
-% så lär behöva fixa, find(textTest==0) = [] gissar jag skulle kunna
-% fungera på ngt sätt
-% 
-%  i = 1;
-%  while i < length(textTest)
-%       if textTest(i) == 32
-%          textTest(i) = [];
-%      end
-%      i = i + 1;
-%  end
-%  
-%Tar bort mellanslag
+textTest = textAsc;    % använder mig utav textTest för att kunna dekryptera från originaltext sedan
+textTestPaj = textAsc; % skapar en ny för att underlätta
 textTest = textTest(find(textTest~=32));
-
- 
-% Sorterar i storleksordning
 textTest = sort(textTest);
 
-% mode kollar det mest frekventa tecknet i arrayn och jag kollar
-% efter det sedan tar jag bort den och alla andra förekomster från textTest
-% för att kunna skaffa en samling av mest frekventa bokstäver
+% samlar de mest frekventa bokstäverna m.h.a mode
+% hade kunnat lägga in om antal , i = -antal
 throw = [];
 i = 1;
 while i < length(textTest) % den fortsätter tills det är bara bokstäver som förekommer lika många ggr eller väldigt få jag vet inte riktigt
@@ -52,37 +28,31 @@ while i < length(textTest) % den fortsätter tills det är bara bokstäver som f
         throw = [throw M]; % lägger in dem i en egen array och använder nu längre inte textTest
     end
     i = i + 1;
-end     
+end
 
-% för k från 1 till antal frekventa bokstäver jag samlade på mig så testar
-% den från E som är 69 i ASCII, som är den mest frekventa bokstaven. Här
-% samlar jag då ett gäng rullningar som jag sedan testar flera av, ifall E
-% inte var det som förekom flest.
+% här samlar jag då ett gäng rullningar som jag sedan testar flera av, ifall E
+% inte var den som förekom flest ggr i texten.
 for k = 1:length(throw)
     throw(k) = (throw(k) - 69);
 end
 
-% skapar bara en vektor med alla rullningar som heter rullning, onödig
-% egentligen
 rullning = [];
 for k = 1:length(throw)
     rullning = [rullning throw(k)];
 end
 
-% skapar en matris med nollor som har lika många rader som rullningar, så
-% att jag kan testa flera olika rullningar i samma matris sedan men på
-% separata rader, längd av hela texten också
+% skapar en matris med nollor som har lika många rader som antal rullningar
+% pre-allocation FTW
 dek = zeros(length(rullning), length(textAsc));
 
 % huvud-loopen håller på tills jag har slut på rullningar att försöka men
 % loopen inuti kör hela texten, där den då byter ut ASCII i originaltexten
 % mot ASCII ifrån mina rullningar, if-else och det är för att de rullar
-% över från Z till B t.ex. och hade fått en ASCII som ej stämde, alfabetet
-% går från A=65 till Z=90
+% över från Z till B t.ex. och hade fått en ASCII som ej stämde.
 i = 1;
 while i <= length(rullning)
     for k = 1:length(textAsc)
-        if textAsc(k)>64 && textAsc(k)<91%rullar bara om värdet motsvarar en versal bokstav.                               
+        if textAsc(k)>64 && textAsc(k)<91%rullar bara om värdet motsvarar en versal bokstav.
           dek(i,k) = textAsc(k) - rullning(i); % rullar bak k:te elementet på den raden som stämmer med den specifika rullningen
           if dek(i,k) <= 64
               dek(i,k) = dek(i,k) + 26;
@@ -96,8 +66,7 @@ while i <= length(rullning)
     i = i + 1;
 end
 
-
- textTestPaj = textTestPaj(find(textTestPaj~=32));
+textTestPaj = textTestPaj(find(textTestPaj~=32));
 
 throw=[];
 for k = 1:10 %hittar de 10 vanligaste
@@ -109,16 +78,14 @@ for k = 1:10 %hittar de 10 vanligaste
     textTestPaj(textTestPaj==M) = []; %tar bort bokstaven som den lade in i throw
 end
 
-
 for k = 1:10
     M = mode(throw); %tar den vanligaste bokstaven i throw
     namn(k) = M; %lägger till den i namn som ASCII
     L = length(throw==M); %antalet av bokstaven
-    delar(k) = L ; %denna ger proportionerna på diagrammet 
+    delar(k) = L ; %denna ger proportionerna på diagrammet
     throw(throw==M) = []; %tar bort fråm throw
     labels{1,k} = char(namn(k)); %lägger in bokstäverna i en cell arrray
 end
 
-pie(delar, labels) 
-
+pie(delar, labels)
 disp(char(dek))
